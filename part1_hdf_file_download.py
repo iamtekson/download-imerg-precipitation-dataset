00e415cb34a59a479b229c0f521e15f2bcb11521
Tek Kshetri <39838116+iamtekson@users.noreply.github.com>
2024-07-15 22:17:15 -0600

    Update part1_hdf_file_download.py

--- part1_hdf_file_download.py
+++ part1_hdf_file_download.py
@@ -15,7 +15,7 @@ for link in hdflinks:
     print(link)
     with requests.Session() as session:
         req = session.request('get', link)
-        r = session.get(req.url, auth=('tekson', '@mZY6NWrcrk#F7w'))
+        r = session.get(req.url, auth=('username', 'password'))
         imagename = link.split('.')
         
         with open('./HDF5/'+imagename[9][:14]+'.HDF5', 'wb') as f:
